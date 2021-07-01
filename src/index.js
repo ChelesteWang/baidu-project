26e6cdef6f4fbb12e31b347c94474677c53b4969
yuqing521 <1581446178@qq.com>
2021-07-01 15:41:31 +0800

    feat: 完成大部分布局

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById('menu');
+function start() {
+    let welcome = document.getElementById("welcome");
+    welcome.style.display = "none";
+    menu.style.display = "none"
+}
